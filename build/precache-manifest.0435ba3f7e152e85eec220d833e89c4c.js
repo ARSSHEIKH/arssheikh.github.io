2ecee4cb7e22406667d8b8ad9321b33f5ccca012
Arsalan Sheikh <41226869+ARSSHEIKH@users.noreply.github.com>
2020-10-04 03:45:09 +0500

    Add files via upload

---
+++ build/precache-manifest.0435ba3f7e152e85eec220d833e89c4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8457b1c2df5f3d77e4e5b33632d4a87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df6a3585cde9dfc550ef",
+    "url": "/static/css/main.b11eaca9.chunk.css"
+  },
+  {
+    "revision": "d1b2c479a2f41fbf62af",
+    "url": "/static/js/2.ffa2648b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ffa2648b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df6a3585cde9dfc550ef",
+    "url": "/static/js/main.4f583dac.chunk.js"
+  },
+  {
+    "revision": "f80722c26d7fbe8d2608",
+    "url": "/static/js/runtime-main.9f7fbb3e.js"
+  }
+]);
